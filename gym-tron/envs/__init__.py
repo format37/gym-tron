b09a1c0adc82e0d7f85dbffbae904da27435c593
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2019-11-08 19:05:04 +0300

    Add files via upload

---
+++ gym-tron/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_lex.envs.lex-tron import LexEnv
---
+++ gym-tron/envs/lex-tron.py
@@ -0,0 +1,15 @@
+class LexEnv(gym.Env):
+
+	def __init__(self):        
+
+	def step(self, action):
+		return True
+
+	def reset(self):
+		return True
+
+	def render(self, mode='human'):
+		return True
+
+	def close(self):
+		a=0
